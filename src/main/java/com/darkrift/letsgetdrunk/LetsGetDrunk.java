4cea80691a14881b82395b3c68246560d616a8ed
darkrift <alexis.lecervoisier@gmail.com>
2015-03-04 12:56:12 +0100

    added config framework

--- src/main/java/com/darkrift/letsgetdrunk/LetsGetDrunk.java
+++ src/main/java/com/darkrift/letsgetdrunk/LetsGetDrunk.java
@@ -1,7 +1,10 @@
 package com.darkrift.letsgetdrunk;
 
+import com.darkrift.letsgetdrunk.handler.ConfigurationHandler;
 import com.darkrift.letsgetdrunk.proxy.IProxy;
 import com.darkrift.letsgetdrunk.reference.Reference;
+import com.darkrift.letsgetdrunk.utility.LogHelper;
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
@@ -9,30 +12,32 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, guiFactory = Reference.GUI_FACTORY_CLASS)
 public class LetsGetDrunk
 {
     @Mod.Instance(Reference.MOD_ID)
     public static LetsGetDrunk instance;
 
-    @SidedProxy(clientSide = "com.darkrift.letsgetdrunk.proxy.ClientProxy", serverSide = "com.darkrift.letsgetdrunk.proxy.ServerProxy")
+    @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
     public static IProxy proxy;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
-
+        ConfigurationHandler.init(event.getSuggestedConfigurationFile());
+        FMLCommonHandler.instance().bus().register(new ConfigurationHandler());
+        LogHelper.info("Pre Initialization Complete");
     }
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event)
     {
-
+        LogHelper.info("Initialization Complete");
     }
 
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event)
     {
-
+        LogHelper.info("Post Initialization Complete");
     }
 }
---
+++ src/main/java/com/darkrift/letsgetdrunk/client/gui/ModGuiConfig.java
@@ -0,0 +1,21 @@
+package com.darkrift.letsgetdrunk.client.gui;
+
+import com.darkrift.letsgetdrunk.handler.ConfigurationHandler;
+import com.darkrift.letsgetdrunk.reference.Reference;
+import cpw.mods.fml.client.config.GuiConfig;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.common.config.ConfigElement;
+import net.minecraftforge.common.config.Configuration;
+
+public class ModGuiConfig extends GuiConfig
+{
+    public ModGuiConfig(GuiScreen guiScreen)
+    {
+        super(guiScreen,
+                new ConfigElement(ConfigurationHandler.configuration.getCategory(Configuration.CATEGORY_GENERAL)).getChildElements(),
+                Reference.MOD_ID,
+                false,
+                false,
+                GuiConfig.getAbridgedConfigPath(ConfigurationHandler.configuration.toString()));
+    }
+}
